5c75d41babbe7db9faf8d4883f868d337338d0f8
Auguste-Houndjago <augustehoundjago@gmail.com>
2024-08-07 07:57:29 +0000

    up

---
+++ app/(front)/Loading.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import '@Styles/Spinner.css'; 
+
+const Loading: React.FC = () => {
+  return <div className="spinner"></div>;
+};
+
+export default Loading;
--- app/layout.tsx
+++ app/layout.tsx
@@ -39,6 +39,7 @@ export default function RootLayout({
               <div className="min-h-screen flex flex-col">
                 <Header />
                 <div><WelcomeModal/></div>
+
                 {children}
                 <footer className="footer footer-center p-4 bg-base-300 text-base-content">
                   <p>
